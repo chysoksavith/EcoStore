7ff3c26ee01d8afceae58d6c1eed061142cb8681
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-07 11:40:56 +0700

    finish detail page part1

---
+++ app/src/main/java/com/example/ecostore/adapter/DetailedActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecostore.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecostore.R;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
